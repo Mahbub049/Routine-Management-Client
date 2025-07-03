d75dd77011106d149b59fd43ad4c6ec59c36e4ba
Mahbub049 <mahbubsarwar49@gmail.com>
2025-07-03 13:28:15 +0600

    link updated

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000", // backend server
+  baseURL: "https://routine-management-server.onrender.com", // backend server
 });
 
 instance.interceptors.request.use((config) => {
